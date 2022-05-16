cb3731384e29290e1b91766804a8220dfcf85f2a
Cast4nha <felipevcast@unifesspa.edu.br>
2022-05-16 17:01:45 -0300

    First commit

---
+++ DIO_Aula02/src/main.java
@@ -0,0 +1,53 @@
+public class main {
+    public static void main(String[] args) {
+
+        byte a = 10;
+        byte b = 0;
+
+        short c = 32;
+        short d = -32;
+
+        int e = 200;
+        int f = -200;
+
+        long g = 100000000000000L;
+        long h = 300000000222111L;
+
+        double i = 3.40423;
+        double j = 8.12312312443;
+
+        float k = 2.400000000F;
+        float l = 12000.401231400000F;
+
+        char m = '\u0129';
+        char n = '\u0064';
+
+        String o = "O";
+        String p = "P";
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+        System.out.println(n);
+        System.out.println(o);
+        System.out.println(p);
+
+
+
+
+
+
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn12301023;
+        //int asrn12301023_78md;
+        //int asrn1230$1023_78md = 10;
+        //int asrn$1230%1023_78md = 10;
+
+        int quantidadeProduto = 60;
+        //int QuantidadeProduto = 40;
+        final int NUMERO_TENTATIVAS = 200;
+        //int QUANTIDADE_OPCOES = 25;
+        //int qtdproduto;
+
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println(j);
+        //System.out.println(asrn12301023);
+        System.out.println(quantidadeProduto);
+
+    }
+}
